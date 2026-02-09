7053abb1c08e5a69b6ea0adf123e33a3f7370fa2
Diallo Abdel <proaziz.diallo@gmail.com>
2026-02-09 10:28:59 +0100

    Premier

---
+++ src/test/java/com/example/automation/runners/RunnerTest.java
@@ -0,0 +1,30 @@
+package com.example.automation.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {
+                "com.example.automation.steps",
+                "com.example.automation.utils",
+                "com.example.automation.configuration"
+        },
+        tags = '@connexion',
+        plugin = { "pretty", "html:target/cucumber-reports.html","json:target/cucumber.json" },
+        monochrome = true
+)
+public class RunnerTest {
+
+    /*@BeforeClass
+    public static void exportXray() {
+        ManageXray.downloadFeatureFiles("POEI2-700");
+    }
+
+    @AfterClass
+    public static void importXray() throws IOException, NoSuchAlgorithmException, KeyStoreException, InterruptedException, KeyManagementException {
+        ManageXray.remonterResultatsXray();
+    }*/
+}
