ec379f7f8e72e46ae30c2b0a4fd7f1edda2ed1a4
Diallo Abdel <proaziz.diallo@gmail.com>
2026-02-09 11:09:58 +0100

    runner modified

--- src/test/java/com/example/automation/runners/RunnerTest.java
+++ src/test/java/com/example/automation/runners/RunnerTest.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
                 "com.example.automation.utils",
                 "com.example.automation.configuration"
         },
-        tags = '@connexion',
+        tags = "@successfulLogin",
         plugin = { "pretty", "html:target/cucumber-reports.html","json:target/cucumber.json" },
         monochrome = true
 )
